e5776682225a63a0a1dc8ac8590b113e8b69b287
grco <gregoro@gmail.com>
2025-11-30 22:21:57 +0000

    Update daily-dashboard to v0.1.1

--- Formula/daily-dashboard.rb
+++ Formula/daily-dashboard.rb
@@ -1,8 +1,8 @@
 class DailyDashboard < Formula
   desc "Terminal-based dashboard for news feeds, weather, and network scanning"
   homepage "https://github.com/grego360/daily-dashboard"
-  url "https://files.pythonhosted.org/packages/1f/fd/c15ca37dbb82b4ba33b2c0bf8097e9ca98f85b71741208ad43fd79f71f4d/daily_dashboard-0.1.0.tar.gz"
-  sha256 "09a4c1ef2cc654fd24230c2237a5720719a8f59c043c80e77fd7d3546b4fa3e9"
+  url "https://files.pythonhosted.org/packages/51/0f/c9276f7dfa47a48fa73e519c87e4966fe9004754af908bcc547a6013f94e/daily_dashboard-0.1.1.tar.gz"
+  sha256 "556b0dd997ac332b2308a285b87ddc35edd5ac7f722c43a5a9ecaa7706a7a5f3"
   license "MIT"
 
   depends_on "python@3.12"
